3911377bb4bfd38b55dcb2f3dc6ce1a6046035e4
James Kebinger <james.kebinger@prefab.cloud>
2025-10-06 17:25:35 -0500

    Remove test scripts/output

--- test-info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export NPM_AUTH_TOKEN=dummy
-export NODE_ENV=test
-export REFORGE_DOMAIN=goatsofreforge.com
-yarn test test/commands/info.test.ts "$@"
--- test-list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export NPM_AUTH_TOKEN=dummy
-export NODE_ENV=test
-yarn test test/commands/list.test.ts "$@"
